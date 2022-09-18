969866af8ba5c6d323c7b7b312210c04db656895
colmpat <cplang@dons.usfca.edu>
2022-09-18 16:02:17 -0700

    Updatad topbar links

--- components/topbar/Topbar.jsx
+++ components/topbar/Topbar.jsx
@@ -1,23 +1,16 @@
 import styles from "./topbar.module.scss"
-import Link from "next/link"
 
 export default function Topbar() {
   return (
     <div className={styles.topbar}>
         <div className={styles.wrapper}>
             <div className={styles.left}>
-                <Link href="/">
-                    <a className={styles.logo}>cpl.</a>
-                </Link>
+                <a href='/#home' className={styles.logo}>cpl.</a>
             </div>
             <div className={styles.right}>
                 <ul>
-                    <Link href="/#intro">
-                        <a><li>01. <span>Intro</span></li></a>
-                    </Link>
-                    <Link href="/#projects">
-                        <a><li>02. <span>Featured Projects</span></li></a>
-                    </Link>
+                    <a href="/#intro"><li>01. <span>Intro</span></li></a>
+                    <a href="/#projects"><li>02. <span>Featured Projects</span></li></a>
                     <a href="https://resume.io/r/wSh41saKj" target="_blank" className={styles.button + " button"}>Resume</a>
                 </ul>
             </div>
